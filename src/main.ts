c914907ed24bb9d2eec24f444c663848239e042b
Binod Chaudhary <thebinod7@gmail.com>
2023-11-20 10:57:26 +0545

    update log and todos

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+
+const PORT = 3333;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // For field validation
-  await app.listen(3333);
+  await app.listen(PORT);
+  Logger.log(`Server running at port: ${PORT}`);
 }
 bootstrap();
